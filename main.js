463b80ab1f685185d8775354391ea0ccc162da0e
MartinKratochvilProgramy <martvil96@gmail.com>
2022-12-16 13:16:36 +0100

    better sizes for mobile

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 var prevScrollpos = window.pageYOffset;
 const navbarEl = document.getElementById("navbar")
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     navbarEl.style.top = "0";
@@ -10,7 +10,7 @@ window.onscroll = function() {
     navbarEl.style.top = `-${navbarEl.offsetHeight}px`;
   }
   prevScrollpos = currentScrollPos;
-} 
+}
 
 // ********** SHOW NAME ON SCROLL **********
 const nameEl = document.getElementById("name");
@@ -22,20 +22,19 @@ const aboutElHeight = aboutEl.offsetHeight;
 
 // ********** SHOW PROJECTS ON SCROLL **********
 const projectsEl = document.getElementById("projects");
-const projectsElHeight = projectsEl.offsetHeight;
 
 // ********** WINDOW HEIGHT **********
 const viewHeight = window.innerHeight;
 
 var myScrollFunc = function () {
-    var y = window.scrollY + viewHeight;
-    if (y >= nameElHeight * 1.2) {
-      aboutEl.className = "about-shown";
-    }
-    console.log(y, aboutElHeight);
-    if (y >=  nameElHeight + aboutElHeight * 1.2) {
-      projectsEl.className = "projects-shown";
-    }
+  var y = window.scrollY + viewHeight;
+  if (y >= nameElHeight * 1.2) {
+    aboutEl.className = "about-shown";
+  }
+  console.log(y, aboutElHeight);
+  if (y >= nameElHeight + aboutElHeight * 1.7) {
+    projectsEl.className = "projects-shown";
+  }
 };
 
 window.addEventListener("scroll", myScrollFunc);
@@ -46,7 +45,7 @@ const email = document.getElementById("email");
 const emailCopyIcon = document.getElementById("email-copy-icon");
 const emailCopied = document.getElementById("email-copied");
 
-emailCopyIcon.onclick = function() {
+emailCopyIcon.onclick = function () {
   document.execCommand("copy");
   emailCopied.classList.add('contact-copied-display')
   setTimeout(() => {
